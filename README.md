832af7424666b730f459cb3f5f507d96a7e035c4
VHSgunzo <wosganzo@gmail.com>
2022-10-10 18:48:49 +0300

    v5.2.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gawk-static
-Statically compiled gawk with musl
+* Statically compiled gawk with musl
+## To get started:
+* **Download the latest revision**
+```
+git clone https://github.com/VHSgunzo/gawk-static.git
+```
+* **Build**
+```
+cd gawk-static && ./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+export MAKEFLAGS="-j$(nproc)"
+
+# WITH_UPX=1
+# NO_SYS_MUSL=1
+
+gawk_version="latest"
+musl_version="latest"
+
+platform="$(uname -s)"
+platform_arch="$(uname -m)"
+
+[ "$gawk_version" == "latest" ] && \
+  gawk_version="$(curl -s https://ftp.gnu.org/gnu/gawk/|tac|\
+                       grep -om1 'gawk-.*\.tar\.xz'|cut -d'>' -f2|sed 's|gawk-||g;s|.tar.xz||g')"
+
+[ "$musl_version" == "latest" ] && \
+  musl_version="$(curl -s https://www.musl-libc.org/releases/|tac|grep -v 'latest'|\
+                  grep -om1 'musl-.*\.tar\.gz'|cut -d'>' -f2|sed 's|musl-||g;s|.tar.gz||g')"
+
+if [ -d build ]
+    then
+        echo "= removing previous build directory"
+        rm -rf build
+fi
+
+if [ -d release ]
+    then
+        echo "= removing previous release directory"
+        rm -rf release
+fi
+
+# create build and release directory
+mkdir build
+mkdir release
+pushd build
+
+# download tarballs
+echo "= downloading gawk v${gawk_version}"
+curl -LO https://ftp.gnu.org/gnu/gawk/gawk-${gawk_version}.tar.gz
+
+echo "= extracting gawk"
+tar -xf gawk-${gawk_version}.tar.gz
+
+if [ "$platform" == "Linux" ]
+    then
+        echo "= setting CC to musl-gcc"
+        if [[ ! -x "$(which musl-gcc 2>/dev/null)" || "$NO_SYS_MUSL" == 1 ]]
+            then
+                echo "= downloading musl v${musl_version}"
+                curl -LO https://www.musl-libc.org/releases/musl-${musl_version}.tar.gz
+
+                echo "= extracting musl"
+                tar -xf musl-${musl_version}.tar.gz
+
+                echo "= building musl"
+                working_dir="$(pwd)"
+
+                install_dir="${working_dir}/musl-install"
+
+                pushd musl-${musl_version}
+                env CFLAGS="$CFLAGS -Os -ffunction-sections -fdata-sections" LDFLAGS='-Wl,--gc-sections' ./configure --prefix="${install_dir}"
+                make install
+                popd # musl-${musl-version}
+                export CC="${working_dir}/musl-install/bin/musl-gcc"
+            else
+                export CC="$(which musl-gcc 2>/dev/null)"
+        fi
+        export CFLAGS="-static -static-libgcc -static-libstdc++"
+        export LDFLAGS='--static'
+    else
+        echo "= WARNING: your platform does not support static binaries."
+        echo "= (This is mainly due to non-static libc availability.)"
+fi
+
+echo "= building gawk"
+pushd gawk-${gawk_version}
+env CFLAGS="$CFLAGS -g -O2 -Os -ffunction-sections -fdata-sections" LDFLAGS="$LDFLAGS -Wl,--gc-sections" ./configure
+make
+popd # gawk-${gawk_version}
+
+popd # build
+
+shopt -s extglob
+
+echo "= extracting gawk binary"
+cp build/gawk-${gawk_version}/gawk release 2>/dev/null
+
+echo "= striptease"
+for file in release/*
+  do
+      strip -s -R .comment -R .gnu.version --strip-unneeded "$file" 2>/dev/null
+done
+
+if [[ "$WITH_UPX" == 1 && -x "$(which upx 2>/dev/null)" ]]
+    then
+        echo "= upx compressing"
+        for file in release/*
+          do
+              upx -9 --best "$file" 2>/dev/null
+        done
+fi
+
+echo "= create release tar.xz"
+tar --xz -acf gawk-static-v${gawk_version}-${platform_arch}.tar.xz release
+
+if [ "$NO_CLEANUP" != 1 ]
+    then
+        echo "= cleanup"
+        rm -rf release build
+fi
+
+echo "= gawk v${gawk_version} done"
